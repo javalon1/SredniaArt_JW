987b85f90934bcda9af0fe6888d6117e29b0e3d0
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-12 15:30:03 +0100

    Srednia_rozwiazane

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,20 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ilosc liczb teraz: ");
+        int n = scanner.nextInt();
+        int wynik = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.println("Podaj liczbe teraz: ");
+            int a = scanner.nextInt();
+            wynik = wynik + a;
+        }
+        System.out.println("Srednia arytmetyczna wynosi: " + wynik/n);
+
+
     }
 }
